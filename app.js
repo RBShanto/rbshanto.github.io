96aa7bf6003814b03fb6a7d2c732d57b5d3887b2
RBShanto <87535911+RBShanto@users.noreply.github.com>
2023-01-03 14:42:52 +0600

    add menu for mobile size

---
+++ app.js
@@ -0,0 +1,7 @@
+const hum = document.querySelector('#header .nav-bar .nav-list .hum');
+const mobile_manu =document.querySelector('#header .nav-bar .nav-list ul');
+const header = document.querySelector('#header .contaniner');
+hum.addEventListener('click',()=>{
+    hum.classList.toggle('active');
+    mobile_manu.classList.toggle('active');
+})
